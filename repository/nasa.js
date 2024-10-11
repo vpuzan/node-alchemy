6c4217a2dd5ef625c74ed22180aa5a2a07935ec1
Vasili Puzan <vasili.puzan@godel.shellenergy.co.uk>
2024-10-11 15:04:49 +0200

    Task-9: Add parameters to the query
    fix comments

--- repository/nasa.js
+++ repository/nasa.js
@@ -5,9 +5,8 @@ import {config} from '../config/config.js';
 export const fetchMeteors = async (startDate, endDate) => {
     const params = {
         start_date: startDate,
-        end_date: endDate,
-        api_key: config.NASA_API_KEY,
+        end_date: endDate
     };
-    const response = await axiosInstance.get('feed', {params});
+    const response = await axiosInstance.get('', {params});
     return response.data;
 };
