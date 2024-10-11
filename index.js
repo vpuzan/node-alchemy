11364c5a6a6b046963dacb0f38820b55addc1720
Vasili Puzan <vasili.puzan@godel.shellenergy.co.uk>
2024-10-11 15:13:18 +0200

    Task-10: Add templated response in parallel with API endpoint

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import express from 'express';
 import {config} from "./config/config.js";
 import controller from './delivery/controller.js'
 import errorHandler from './exception/error_middleware.js';
+import nunjucks from 'nunjucks';
 
 const app = express();
 
 app.use(controller)
 app.use(errorHandler)
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: app
+})
+
 
 app.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
