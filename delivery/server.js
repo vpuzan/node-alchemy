d16e6f7cc48efd08a37a56d4e9f3f8ddf1248841
Vasili Puzan <vasili.puzan@godel.shellenergy.co.uk>
2024-10-11 13:01:37 +0200

    Task-9: Add parameters to the query
    fix comments

--- delivery/server.js
+++ delivery/server.js
@@ -3,6 +3,7 @@ import {getSortedAndFilteredMeteors} from '../use_case/meteors.js';
 import {config} from '../config/config.js';
 import Exception from "../exception/exception.js";
 import errorHandler from "../exception/error_middleware.js";
+import nunjucks from 'nunjucks';
 
 const server = express();
 
@@ -23,13 +24,19 @@ server.get('/meteors', async (req, res, next) => {
     try {
         const {startDate, endDate, count, wereDangerousMeteors } = req.query;
         const filteredData = await getSortedAndFilteredMeteors(startDate, endDate, isBoolean(count), isBoolean(wereDangerousMeteors));
-        res.json(filteredData);
+        // res.json(filteredData);
+        res.render('meteors.njk', filteredData);
     } catch (error) {
         next(new Exception(error.status, `Error fetching data: ${error.message}`));
     }
 });
 
 server.use(errorHandler);
+
+nunjucks.configure('templates', {
+    autoescape: true,
+    express: server
+})
 
 server.listen(config.port, () => {
     console.log(`Server is running on port ${config.port}`);
