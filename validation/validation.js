2a87b9036b556b169e1ab52b128515c607a461df
Vasili Puzan <vasili.puzan@godel.shellenergy.co.uk>
2024-10-14 16:42:37 +0200

    Task-12: Add validation
    We choose to use such a wonderful library as Joi.
    
    fix comments

--- validation/validation.js
+++ validation/validation.js
@@ -1,14 +1,9 @@
 import Exception from "../exception/exception.js";
 
 export const validateRequest = (schema) => (req, res, next) => {
-    let error;
-    if (Object.keys(req.body).length === 0){
-        error = schema.validate(req.query);
-    } else{
-        error = schema.validate(req.body);
-    }
-    if (error.error) {
-        throw new Exception(400, error.error.stack);
+    const {error} = req.method === 'GET' ? schema.validate(req.query) : schema.validate(req.body);
+    if (error) {
+        next(new Exception(400, error.stack));
     }
     next();
 };
