50f85e74f355f21d36c3953535aa5305689c0aba
Vasili Puzan <vasili.puzan@godel.shellenergy.co.uk>
2024-10-18 16:51:20 +0200

    Task-15: Refactor to typescript Acceptance criteria:
    Type script is added and configured
    Implement build and run with TS support
    All files are refactored to TS
    Types and models are covered with types

---
+++ src/config/config.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  NASA_BASE_URL,
+  METEORS_API_URL,
+  PHOTOS_ROVER_API_URL,
+  NASA_API_KEY,
+  PORT,
+  SOL,
+  CAMERA,
+} = process.env || (() => {
+  throw new Error('Missing env keys');
+})();
+
+export const configApp: {
+  port: string;
+  nasaBaseUrl: string;
+  meteorsUrl: string;
+  photoRoverUrl: string;
+  sol?: string;
+  camera?: string;
+} = {
+  port: PORT!,
+  nasaBaseUrl: NASA_BASE_URL!,
+  meteorsUrl: `${METEORS_API_URL}?api_key=${NASA_API_KEY}`,
+  photoRoverUrl: `${PHOTOS_ROVER_API_URL}`,
+  sol: SOL,
+  camera: CAMERA,
+};
